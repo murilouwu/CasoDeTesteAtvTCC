f9ac1606dfb75658c86c8a227e3f225f5456f69d
Murilo Gimenez <91744500+murilouwu@users.noreply.github.com>
2023-09-09 18:26:35 -0300

    Add files via upload

---
+++ assets/vars.js
@@ -0,0 +1,34 @@
+/*
+    0-Titulo do caso.
+    1-Descrição do caso
+    2-Pré condições
+    3-tabela
+        3,1 - Ações do passo
+        3,2 - Resultado esperado
+*/
+
+const CaseItem = [];
+CaseItem[0] = [
+    'Titulo',
+    'Descrição',
+    [
+        'Pré condição',
+        'Pré condição2',
+        'Pré condição3',
+        '...'
+    ],
+    [
+        [
+            'Ação1',
+            'Resultado1'
+        ],
+        [
+            'Ação2',
+            'Resultado2'
+        ],
+        [
+            '...',
+            '...'
+        ],
+    ]
+];
